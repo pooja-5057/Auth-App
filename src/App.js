fd2c778441142e31172df958f3f2356981d43e3b
pooja-5057 <pooja.borse.5057@gmail.com>
2025-06-23 09:51:23 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login with Google or Facebook</h1>
+    
+    </div>
+  );
+}
+
+export default App;
